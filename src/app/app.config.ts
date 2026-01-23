669bec610289ce02c4d2dbe35d98092d2e7eb7d4
Bato05 <baturodriguez05@gmail.com>
2026-01-23 15:39:56 -0300

    cargar carpetas del proyecto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    HttpClient,
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
