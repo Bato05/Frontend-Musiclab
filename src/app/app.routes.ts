433e149f3e6ca69f2e978515e66b32fab049f2f1
Bato05 <baturodriguez05@gmail.com>
2026-01-24 16:00:50 -0300

    Desarrollando el formulario de login y de registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './components/forms/login/login';
+import { Register } from './components/forms/register/register';
+
+export const routes: Routes = [
+    // Redirecciones...
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'login', component: Login},
+    { path: 'register', component: Register},
+];
---
+++ src/app/components/forms/register/register.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { RouterLink, RouterLinkActive } from '@angular/router'; 
+
+@Component({
+  selector: 'app-register',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+
+}
